259547a75a7061df3688c68af25e526348f8b1a2
Vicente Herrera <127439959+Ventzzz@users.noreply.github.com>
2025-12-28 01:59:30 -0300

    Refina estilos base del portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import './App.css'
 import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <>
-      <Portfolio />
-      <div className="bg-red-500 text-white p-6 text-xl">
-        Tailwind está funcionando
-      </div>
-    </>
+    <Portfolio />
   );
 }
 
